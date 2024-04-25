2c214f7190a24387e01cac99c9bd194d05d0b6fc
NikosGour <nikosgournak@gmail.com>
2024-04-25 20:41:31 +0300

    docs: add javadocs to UTC_to_local

--- src/main/java/gr/hua/dit/nikosgourn/Main.java
+++ src/main/java/gr/hua/dit/nikosgourn/Main.java
@@ -1,4 +1,6 @@
 package gr.hua.dit.nikosgourn;
+import org.jetbrains.annotations.NotNull;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -16,14 +18,18 @@ public class Main
         Path filepath = Path.of(System.getProperty("user.dir") , "pom.xml");
         BasicFileAttributes attr = Files.readAttributes(filepath , BasicFileAttributes.class);
         FileTime last_access = attr.lastModifiedTime();
-        
+        System.out.println(last_access);
         String date_local = UTC_to_local(last_access.toString());
         System.out.println("File: \"" + filepath + "\"\nLast Modified On: " + date_local);
     }
     
-    private static String UTC_to_local(String dataTime)
+    /**
+     * @param date_time A UTC date_time string: example "2024-04-25T17:33:24.6169298Z"
+     * @return the same time but offset to the systems time-zone + some different formating: example of above "[25-Apr-2024] 20:33:24.616"
+     */
+    private static @NotNull String UTC_to_local(@NotNull String date_time)
     {
-        Instant last_access_utc = Instant.parse(dataTime);
+        Instant last_access_utc = Instant.parse(date_time);
         DateTimeFormatter localFormat =
                 DateTimeFormatter.ofPattern("'['dd-MMM-yyyy']' HH:mm:ss.SSS");
         return last_access_utc.atZone(ZoneId.systemDefault()).format(localFormat);
