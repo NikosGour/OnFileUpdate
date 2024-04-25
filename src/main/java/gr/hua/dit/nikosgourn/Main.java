7ca35ce3e6b445075b4663528d4d651847e7da0f
NikosGour <nikosgournak@gmail.com>
2024-04-25 21:47:36 +0300

    refactor: change some names in UTC_to_local and extract method get_last_modified

--- src/main/java/gr/hua/dit/nikosgourn/Main.java
+++ src/main/java/gr/hua/dit/nikosgourn/Main.java
@@ -1,6 +1,8 @@
 package gr.hua.dit.nikosgourn;
+
 import org.jetbrains.annotations.NotNull;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,26 +15,36 @@ import java.time.format.DateTimeFormatter;
 
 public class Main
 {
-    public static void main(String[] args) throws IOException, ParseException
-    {
-        Path filepath = Path.of(System.getProperty("user.dir") , "pom.xml");
-        BasicFileAttributes attr = Files.readAttributes(filepath , BasicFileAttributes.class);
-        FileTime last_access = attr.lastModifiedTime();
-        System.out.println(last_access);
-        String date_local = UTC_to_local(last_access.toString());
-        System.out.println("File: \"" + filepath + "\"\nLast Modified On: " + date_local);
-    }
-    
-    /**
-     * @param date_time A UTC date_time string: example "2024-04-25T17:33:24.6169298Z"
-     * @return the same time but offset to the systems time-zone + some different formating: example of above "[25-Apr-2024] 20:33:24.616"
-     */
-    private static @NotNull String UTC_to_local(@NotNull String date_time)
-    {
-        Instant last_access_utc = Instant.parse(date_time);
-        DateTimeFormatter localFormat =
-                DateTimeFormatter.ofPattern("'['dd-MMM-yyyy']' HH:mm:ss.SSS");
-        return last_access_utc.atZone(ZoneId.systemDefault()).format(localFormat);
-    }
-    
+	public static void main(String[] args) throws IOException, InterruptedException
+	{
+		Path filepath = Path.of(System.getProperty("user.dir") , "pom.xml");
+		while (true){
+//			FileTime last_modified = get_last_modified(filepath);
+			//			System.out.println("File: \"" +filepath + "\"\nLast Modified On: " + last_modified);
+			System.out.println("-".repeat(50));
+			Thread.sleep(3000);
+		}
+	}
+	
+	private static FileTime get_last_modified(Path filepath) throws IOException, InterruptedException
+	{
+		BasicFileAttributes attr = Files.readAttributes(filepath , BasicFileAttributes.class);
+		FileTime last_modified = attr.lastModifiedTime();
+		//		System.out.println(last_modified);
+		String last_modified_local = UTC_to_local(last_modified.toString());
+		System.out.println("File: \"" + filepath + "\"\nLast Modified On: " + last_modified_local);
+		return last_modified;
+	}
+	
+	/**
+	 * @param date_time A UTC date_time string: example "2024-04-25T17:33:24.6169298Z"
+	 * @return the same time but offset to the systems time-zone + some different formating: example of above "[25-Apr-2024] 20:33:24.616"
+	 */
+	private static @NotNull String UTC_to_local(@NotNull String date_time)
+	{
+		Instant date_time_utc = Instant.parse(date_time);
+		DateTimeFormatter localFormat = DateTimeFormatter.ofPattern("'['dd-MMM-yyyy']' HH:mm:ss.SSS");
+		return date_time_utc.atZone(ZoneId.systemDefault()).format(localFormat);
+	}
+	
 }
